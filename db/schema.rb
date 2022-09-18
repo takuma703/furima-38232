b3fc1a1c78d0c1823c032e5cc568688b8eb41901
takuma703 <takuina.km@gmail.com>
2022-09-18 17:34:43 +0900

    ユーザー認証機能の実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_16_135145) do
+ActiveRecord::Schema.define(version: 2022_09_17_125113) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "first_name", null: false
