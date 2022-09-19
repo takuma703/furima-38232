769b5574c6272b604a5f903b9e8aad5f2403e4b5
takuma703 <takuina.km@gmail.com>
2022-09-19 09:26:12 +0900

    バリデーションの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,28 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :password, format: {
+    with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze,
+    message: "は半角英数文字のみでその両方を含む入力が必要です"
+  }
+  validates :first_name, presence: true, format: {
+    with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/,
+    message: "は全角で入力して下さい"
+  }
+  validates :family_name, presence: true, format: {
+    with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/,
+    message: "は全角で入力して下さい"
+  }
+  validates :first_name_kana, presence: true, format: {
+    with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/,
+    message: 'は全角カナで入力して下さい。'
+  }
+  validates :family_name_kana, presence: true, format: {
+    with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/,
+    message: 'は全角カナで入力して下さい。'
+  }
+  validates :birthday, presence: true
+
 end
