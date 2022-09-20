4fe3e7d76a7ebdc594c8e5b13f6a7c639c1cd5ac
takuma703 <takuina.km@gmail.com>
2022-09-20 19:56:32 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,20 @@
 class Item < ApplicationRecord
   has_one_attached :image
-  belongs_to :category
+
+  validates :name, presence: true
+  validates :introduction, presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :item_condition, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :delivery_charge_payer, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :prefecture, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :shipping_date, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :price, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 9_999_999 },format: { with: /\A[0-9]+\z/ }
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :item_condition
+  belongs_to :delivery_charge_payer
+  belongs_to :prefecture
+  belongs_to :shipping_date
 
-
-  validates :genre_id, numericality: { other_than: 1 , message: "can't be blank"} 
 end
