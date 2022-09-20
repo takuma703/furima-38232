b0d5b7efb6ca9a51b11636be3229e20335108d83
takuma703 <takuina.km@gmail.com>
2022-09-20 17:54:34 +0900

    categoryモデルの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,32 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.order("created_at DESC")
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
 
-  def message_params
-    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+  def item_params
+    params.require(:item).permit(
+      :name,
+      :introduction
+      :category_id
+      :item_condition_id
+      :delivery_charge_payer_id
+      :prefecture_id
+      :price
+      :shipping_date_id).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,10 @@
 class Item < ApplicationRecord
   has_one_attached :image
+  belongs_to :category
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+
+
+  validates :genre_id, numericality: { other_than: 1 , message: "can't be blank"} 
 end
